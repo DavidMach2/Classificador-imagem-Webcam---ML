b5628e9a6113fb49da17e8827022e4ae02996b30
DavidMach2 <31442735+DavidMach2@users.noreply.github.com>
2021-10-21 12:34:28 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+// Image Classification with MobileNet
+// A Beginner's Guide to Machine Learning with ml5.js
+// The Coding Train / Daniel Shiffman
+// https://youtu.be/D9BoBSkLvFo
+// https://thecodingtrain.com/learning/ml5/1.2-webcam-classification.html
+// https://editor.p5js.org/codingtrain/sketches/JrudwwVD
+//https://github.com/DavidMach2/website/tree/main/learning/ml5
+//https://p5js.org/
+
+let mobilenet;
+let video;
+let label = '';
+
+function modelReady() {
+  console.log('Model is ready!!!');
+  mobilenet.predict(gotResults);
+}
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    //console.log(results);
+    label = results[0].className;
+    mobilenet.predict(gotResults);
+  }
+}
+
+function setup() {
+  createCanvas(640, 550);
+  video = createCapture(VIDEO);
+  video.hide();
+  background(0);
+  mobilenet = ml5.imageClassifier('MobileNet', video, modelReady);
+}
+
+function draw() {
+  background(0);
+  image(video, 0, 0);
+  fill(255, 255, 0);
+  textSize(32);
+  text(label, 10, height - 20);
+}
